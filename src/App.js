aabcad75e30c50e6addde09409833918d715460b
Anthony <anthonygibson24@gmail.com>
2022-05-25 23:51:10 -0500

    setup all route components & home route on App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from "./components/Home"
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/Home";
+import Number from "./components/Number"
+import Word from "./components/Word"
+import WordColorNumber from "./components/WordColorBackground"
+import {BrowserRouter as Router, Switch, Route, Link} from "react-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Routes>
-          
-        </Routes>
-      <Home />
+        <Router>
+        <Route exact path ="/home" component={Home}/>
+          <Home />
+        </Router>
       </header>
     </div>
   );
---
+++ src/components/WordColorBackground.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const WordColorBackground = () => {
+
+    const theStyle = {
+        color: {color},
+        backgroundColor: {backgroundColor}
+    }
+    return (
+        <div>
+            <p style={theStyle}>{word}</p>
+        </div>
+    )
+}
